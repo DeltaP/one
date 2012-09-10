7aa39bae8be65377ed46a9adea0644ee7ad345d2
Gregory <gregpetrop@gmail.com>
2012-09-09 23:12:42 -0600

    more comments

--- ring_hello.cpp
+++ ring_hello.cpp
@@ -13,23 +13,28 @@
 const int MAX_STRING = 100;
 
 int main(void) {
-  int        comm_sz;                                   /* Number of processes    */
-  int        my_rank;                                   /* My process rank        */
+  int  comm_sz;                                          /* Number of processes                 */
+  int  my_rank;                                          /* My process rank                     */
   char message[MAX_STRING];
 
   
-  MPI_Init(NULL, NULL);                                 /* Start up MPI */
-  MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);              /* Get the number of processes */
-  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);              /* Get my rank among all the processes */
+  MPI_Init(NULL, NULL);                                  /* Start up MPI                        */
+  MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);               /* Get the number of processes         */
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);               /* Get my rank among all the processes */
 
-  int to   = (my_rank+1)%comm_sz;
-  int from = (my_rank-1+comm_sz)%comm_sz;
-  sprintf(message, "Greetings from process %d of %d", my_rank, comm_sz);
+  int to   = (my_rank+1)%comm_sz;                        /* finds which process to send to      */
+  int from = (my_rank-1+comm_sz)%comm_sz;                /* finds which process to recieve from */
+  sprintf(message, "Greetings from process %d of %d",    /* forms message                       */
+      my_rank, comm_sz);
 
-  MPI_Send(message, strlen(message)+1, MPI_CHAR, to, 0, MPI_COMM_WORLD);
-  MPI_Recv(message, MAX_STRING, MPI_CHAR, from, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  printf("%s | reported by process %d of %d\n", message, my_rank, comm_sz);
+  MPI_Send(message, strlen(message)+1, MPI_CHAR, to, 0,  /* sends mpi message                   */
+      MPI_COMM_WORLD);
+  MPI_Recv(message, MAX_STRING, MPI_CHAR, from, 0,       /* recieves mpi message                */
+      MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+  printf("%s | reported by process %d of %d\n",          /* prints output                       */
+      message, my_rank, comm_sz);
     
-  MPI_Finalize();
+  MPI_Finalize();                                        /* kills mpi                           */
   return 0;
 }
