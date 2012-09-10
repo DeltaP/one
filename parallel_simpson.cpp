f9a4374f714b1090e4b4b3149a1f60ba54df6d92
Gregory <gregpetrop@gmail.com>
2012-09-09 19:37:52 -0600

    pass h instead of calculate it every time

--- parallel_simpson.cpp
+++ parallel_simpson.cpp
@@ -2,3 +2,35 @@
  *This is the parallel version of the Simpson Progra
  *
  */
+
+
+#include <stdio.h>
+#include <mpi.h>
+#include <iostream>
+using namespace std;
+
+
+int main(void) {
+  double a,b,p,q,result;
+  int i,n;
+  
+  MPI_Init(NULL, NULL);                                 /* Start up MPI */
+  MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);              /* Get the number of processes */
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);              /* Get my rank among all the processes */
+
+  if(my_rank==0) {
+    //pprompt for a,b,n
+    cout << "Enter the beginning of the integration interval [a,b] 'a':\n";
+    cin >> a;
+    cout << "Enter the end of the integration interval [a,b] 'b':\n";
+    cin >> b;
+    cout << "Enter the number of intervals 'n':\n";
+    cin >> n;
+  }
+
+  result=0;
+  p=a;
+  q=p+(b-a)/n;
+
+  return 0;
+}
--- serial_simpson.cpp
+++ serial_simpson.cpp
@@ -5,20 +5,20 @@
  */
 
 #include <stdio.h>
+#include <math.h>
 #include <iostream>
 using namespace std;
 
 double f_x(double x) {
   double value;
 
-  value=2*x*x+3*x-15;
+  value=2*pow(x,3)+3*pow(x,2)+x-15;
   return value;
 }
 
-double simpson (double local_p, double local_q) {
-  double integral, h, r;
+double simpson (double local_p, double local_q, double h) {
+  double integral, r;
 
-  h=(local_q-local_p)/2;
   r=local_p+h;
   integral = (h/3)*(f_x(local_p)+4*f_x(r)+f_x(local_q));
 
@@ -26,18 +26,25 @@ double simpson (double local_p, double local_q) {
 }
 
 int main(void) {
-  double a,b,p,q,result;
+  double a,b,p,q,h,result;
   int i,n;
   
-  a=0;
-  b=100;
-  n=100;
-  result=0;
 
+  cout << "Enter the beginning of the integration interval [a,b] 'a':\n";
+  cin >> a;
+
+  cout << "Enter the end of the integration interval [a,b] 'b':\n";
+  cin >> b;
+
+  cout << "Enter the number of intervals 'n':\n";
+  cin >> n;
+
+  result=0;
   p=a;
   q=p+(b-a)/n;
+  h=(q-p)/2;
   for(i=0; i<n; i++) {
-    result+=simpson(p,q);
+    result+=simpson(p,q,h);
     p=q;
     q=p+(b-a)/n;
   }
