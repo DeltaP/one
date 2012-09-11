c858efca84ca4eb5c21e170d413fa14cb6bb626b
Gregory <gregpetrop@gmail.com>
2012-09-11 13:49:19 -0600

    serial simpson is updated to run as intended

--- serial_simpson.cpp
+++ serial_simpson.cpp
@@ -13,6 +13,8 @@
 #include <stdlib.h>
 #include <math.h>
 #include <iostream>
+#include <string>
+#include <sstream>
 using namespace std;
 
 // -----------------------------------------------------------------
@@ -20,7 +22,8 @@ using namespace std;
 double f_x(double x) {
   double value;
 
-  value=2*pow(x,5)-pow(x,4)+5*pow(x,3)-10*pow(x,2)+15*x-3;
+  //value=2*pow(x,5)-pow(x,4)+5*pow(x,3)-10*pow(x,2)+15*x-3;
+  value = pow(x,1);
   return value;
 }
 // -----------------------------------------------------------------
@@ -29,11 +32,29 @@ double f_x(double x) {
 
 // -----------------------------------------------------------------
 // computes the volume of one element
-double simpson (double local_p, double local_q, double h) {
-  double integral, r;
+double simpson (double local_a, int n, double local_h, int count, bool v, string& out) {
+  double integral, x;
+  int coefficient;
 
-  r=local_p+h;
-  integral = (h/3)*(f_x(local_p)+4*f_x(r)+f_x(local_q));
+  x = local_a + count * local_h;
+
+  if ( (count == 0) || (count == n) ) {
+    coefficient = 1;
+  }
+  else if (count % 2 == 0) {
+    coefficient = 2;
+  }
+  else {
+    coefficient = 4;
+  }
+
+  integral = (local_h/3)*coefficient*f_x(x);
+
+  if (v == true) {
+    ostringstream convert;
+    convert << coefficient;
+    out = out + convert.str() + " ";
+  }
 
   return integral;
 }
@@ -54,8 +75,10 @@ void cleanup (const char *message) {
 // -----------------------------------------------------------------
 // the main program
 int main(int argc, char *argv[]) {
-  double a,b,p,q,h,result;
-  int i,n;
+  double a, b, h, result;
+  int i, n;
+  bool verbose = false;
+  string flag, out;
 
   if (argc < 4) {                                       /* too few arguments aborts the program */
     cleanup("Error:  Too few arguments");
@@ -65,20 +88,29 @@ int main(int argc, char *argv[]) {
     b = strtod(argv[2], NULL);
     n = atoi(argv[3]);
   }
-  if (argc > 4) {                                       /* too many arguments aborts the program*/
+  if (argc == 5) {                                      /* option to run with a b n as inputs   */
+    flag = argv[1];
+    if (flag.compare("-verbose") != 0) {
+      cleanup("Error:  Wrong flag, only '-verbose' supported");
+    }
+    else if (flag.compare("-verbose") == 0) {           /* not optimal for one flag went with   */
+      verbose = true;                                   /* general approach                     */
+    }
+    a = strtod(argv[2], NULL);
+    b = strtod(argv[3], NULL);
+    n = atoi(argv[4]);
+  }
+  if (argc > 5) {                                       /* too many arguments aborts the program*/
     cleanup("Error:  Too many arguments");
   }
 
   result = 0;                                           /* calculates the integral result       */
-  p = a;
-  q = p+(b-a)/n;
-  h = (q-p)/2;
-  for (i = 0; i < n; i++) {
-    result += simpson(p,q,h);
-    p = q;
-    q = p+(b-a)/n;
+  h = (b-a)/n;
+  for (i = 0; i < n+1; i++) {
+    result += simpson(a, n, h, i, verbose, out);
   }
 
+  if (verbose == true) { cout << out << endl; }
   cout << "The integral is: " << result << endl;        /* outputs the answer                   */
 
   cleanup("Program Complete");                          /* terminates the program               */
